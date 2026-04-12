4dc315b62c2948deb6f1a2fa1230410b063f8a5e
Shubhangi Vishwakarma <155371171+ShubhiV13@users.noreply.github.com>
2026-04-13 01:04:28 +0530

    Update build.sh

--- build.sh
+++ build.sh
@@ -10,7 +10,7 @@ python manage.py shell << EOF
 from django.contrib.auth.models import User
 if not User.objects.filter(username='admin').exists():
     User.objects.create_superuser('admin', 'admin@example.com', 'admin123')
-    print('✅ Superuser created')
+    print(' Superuser created')
 else:
-    print('✅ Superuser already exists')
+    print(' Superuser already exists')
 EOF
