224265cde58e05623ab0a11d96963599083701cc
Shubhangi Vishwakarma <155371171+ShubhiV13@users.noreply.github.com>
2026-04-13 00:36:49 +0530

    Delete api directory

--- api/urls.py
+++
@@ -1,7 +0,0 @@
-from django.urls import path
-from .views import RegisterView, ProtectedView, home_view
-
-urlpatterns = [
-    path('register/', RegisterView.as_view(), name='register'),
-    path('protected/', ProtectedView.as_view(), name='protected'),
-]
